4daed7649dca1b529278c5a40aa53c852694948f
moti195 <moti195@gmail.com>
2016-02-26 09:27:29 +0200

    the fiirst commit

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
-(function () {
-	// You app starts here
-}());
+(function (angular) {
+	var myApp=angular.module("myApp",["ngRoute"]);
+
+	myApp.factory('postsReader', function($http, $q) {
+		var deferred = $q.defer();
+
+ 		$http({method:'Get', url:"./data/posts.json"}).then(function(data) {
+			deferred.resolve(data.data.posts);
+ 		})
+
+		return deferred.promise; 
+ 	});
+
+ 	myApp.controller('postsController', function($scope, postsReader) {
+ 		postsReader.then(function(data){
+ 			$scope.posts = data;
+ 			$scope.template_url = "./templates/post.html"
+ 		})
+ 	})
+
+
+
+
+
+	
+}(angular));
